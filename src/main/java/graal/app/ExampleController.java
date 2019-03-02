814fe85a4f70df32072e2d16c5878644e29b6201
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2019-03-02 13:40:05 +0100

    updated with httpclient error

--- src/main/java/graal/app/ExampleController.java
+++ src/main/java/graal/app/ExampleController.java
@@ -1,5 +1,13 @@
 package graal.app;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.http.apache.client.impl.ApacheConnectionManagerFactory;
+import com.amazonaws.http.apache.client.impl.ApacheHttpClientFactory;
+import com.amazonaws.http.apache.client.impl.ConnectionManagerAwareHttpClient;
+import com.amazonaws.http.client.ConnectionManagerFactory;
+import com.amazonaws.http.client.HttpClientFactory;
+import com.amazonaws.http.conn.ClientConnectionManagerFactory;
+import com.amazonaws.http.settings.HttpClientSettings;
 import com.amazonaws.partitions.model.Partitions;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -7,8 +15,10 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.HttpClientConnectionManager;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,20 +26,33 @@ import java.io.InputStream;
 @Controller("/")
 public class ExampleController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ExampleController.class);
+    private static final Log LOG = LogFactory.getLog(ExampleController.class);
     private ObjectMapper mapper = new ObjectMapper()
             .disable(MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS)
             .disable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS)
             .enable(JsonParser.Feature.ALLOW_COMMENTS)
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
+    private static final HttpClientFactory<ConnectionManagerAwareHttpClient> httpClientFactory = new
+            ApacheHttpClientFactory();
+
+    private HttpClient httpClient;
+
+    public ExampleController() {
+        //this.httpClient = httpClientFactory.create(HttpClientSettings.adapt(new ClientConfiguration()));
+        LOG.info("init");
+        final ConnectionManagerFactory<HttpClientConnectionManager> cmFactory = new ApacheConnectionManagerFactory();
+        final HttpClientConnectionManager cm = cmFactory.create(HttpClientSettings.adapt(new ClientConfiguration()));
+        ClientConnectionManagerFactory.wrap(cm);
+    }
+
     @Get("/ping")
     public Partitions index() throws IOException {
         LOG.info("Local Test");
-
+        LOG.info(httpClient);
         InputStream stream = ExampleController.class.getClassLoader().getResourceAsStream("com/amazonaws/partitions/endpoints.json");
         final Partitions partitions = mapper.readValue(stream, Partitions.class);
-        LOG.info("Local Test partitions {}", partitions);
+        LOG.info("Local Test partitions " + partitions);
         return partitions;
     }
 }
